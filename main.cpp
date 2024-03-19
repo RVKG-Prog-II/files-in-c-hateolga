86c18854eb74809c1fbe8733af0bbc2bb4cb9ac3
hateolga <162315353+hateolga@users.noreply.github.com>
2024-03-19 14:29:14 +0200

    Update main.cpp
    lgnejk

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
+#include <iostream>
+#include <fstream>
 #include <string>
-// You may include other headers, as needed
 
-using std::string;
-
+using namespace std;
 void countAndWriteDigitsPerLine(string filename, string outputFilename)
 {
-    // Your code here
+ofstream writefile(outputFilename);
+ifstream readfile(filename);
+string line;
+int counter = 0;
+while (getline(readfile, line)) {
+for (int i = 0; i < line.size(); i++) {
+if (line[i] >= '0' && line[i] <= '9') {
+counter++;
 }
-
-unsigned int countMaxDigits(string filename)
-{
-    // Your code here
+}
+writefile << counter << "\n";
+cout << counter << "\n";
+}
+}
+int countMaxDigits(string filename) {
+ifstream readfile(filename);
+string line;
+int b = 0;
+int c = 0;
+int counter = 0;
+while (getline(readfile, line)) {
+for (int i = 0; i < line.size(); i++) {
+if (line[i] >= '0' && line[i] <= '9') {
+b = counter;
+counter++;
+}
+}
+}
+if (counter > b) {
+c = counter;
+}
+cout << c << "\n";
+return c;
+}
+int main() {
+countAndWriteDigitsPerLine("C:\\Users\\RVKG\\Downloads\\filename.txt", "outputFilename.txt");
+countMaxDigits("C:\\Users\\RVKG\\Downloads\\filename.txt");
 }
